9870537ae1dfb7f14abeef98aa3a64c5d1b4ca51
Kent Watsen <kent@watsen.net>
2019-02-22 22:01:18 -0500

    adding -v1

--- yang/gen-tree.sh
+++ yang/gen-tree.sh
@@ -2,8 +2,8 @@
 
 DATE=`date +%Y-%m-%d`
 
-sed -e"s/YYYY-MM-DD/$DATE/" xiax-block.yang > xiax-block\@$DATE.yang
+sed -e"s/YYYY-MM-DD/$DATE/" xiax-block-v1.yang > xiax-block-v1\@$DATE.yang
 
-pyang -f tree --tree-line-length 71 --tree-print-yang-data xiax-block\@*.yang > xiax-block-tree.txt
+pyang -f tree --tree-line-length 71 --tree-print-yang-data xiax-block-v1\@*.yang > tree.txt
 
-rm xiax-block\@*.yang
+rm xiax-block-v1\@*.yang
--- yang/validate.sh
+++ yang/validate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 DATE=`date +%Y-%m-%d`
-sed -e"s/YYYY-MM-DD/$DATE/" xiax-block.yang > xiax-block\@$DATE.yang
+sed -e"s/YYYY-MM-DD/$DATE/" xiax-block-v1.yang > xiax-block-v1\@$DATE.yang
 
 echo
 
-echo "validating xiax-block.yang..."
+echo "validating xiax-block-v1.yang..."
 printf "  ^ with pyang..."
-response=`pyang --strict --canonical --max-line-length=69 xiax-block\@*.yang 2>&1`
+response=`pyang --strict --canonical --max-line-length=69 xiax-block-v1\@*.yang 2>&1`
 if [ $? -ne 0 ]; then
   printf "failed (error code: $?)\n"
   printf "$response\n\n"
@@ -16,7 +16,7 @@ if [ $? -ne 0 ]; then
 fi
 printf "okay.\n"
 printf "  ^ with yanglint..."
-response=`yanglint xiax-block\@*.yang 2>&1`
+response=`yanglint xiax-block-v1\@*.yang 2>&1`
 if [ $? -ne 0 ]; then
   printf "failed (error code: $?)\n"
   printf "$response\n\n"
@@ -26,11 +26,11 @@ fi
 printf "okay.\n\n"
 
 
-echo "validating ex-xiax-block.xml..."
+echo "validating ex-xiax-block-v1.xml..."
 
 # first, we need to create a version of the YANG module without the "yang-data"
 # extension (e.g., make it look like it defines protocol-accessible nodes)
-name="xiax-block\@*.yang"
+name="xiax-block-v1\@*.yang"
 numlines=`wc -l $name | awk '{print $1}'`
 delline=`expr $numlines - 1`  # hope it doesn't move!
 awk "NR%$delline" $name > $name.2
@@ -38,7 +38,7 @@ sed -e '/rc:yang-data/d' $name.2 > $name
 rm $name.2 
 
 printf "  ^ with yanglint..."
-response=`yanglint --verbose --strict xiax-block\@*.yang ex-xiax-block.xml 2>&1`
+response=`yanglint --verbose --strict xiax-block-v1\@*.yang ex-xiax-block-v1.xml 2>&1`
 if [ $? -ne 0 ]; then
   printf "failed (error code: $?)\n"
   printf "$response\n\n"
@@ -49,4 +49,4 @@ fi
 printf "okay.\n"
 
 
-rm xiax-block\@*.yang
+rm xiax-block-v1\@*.yang
